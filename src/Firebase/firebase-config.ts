1362aeb0480a37ed0fcd5542e5dce199645ea48c
Skanda <skanda@sutara.org>
2025-04-08 16:37:11 +0530

    Updated the run command in github actions and hosting command

--- src/Firebase/firebase-config.ts
+++ src/Firebase/firebase-config.ts
@@ -8,3 +8,15 @@ export const firebaseConfig = {
     appId: "1:602402387941:web:a63f4eaddc949f539de10c",
     measurementId: "G-FVLSN7D7NM",
 };
+
+
+// export const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY!,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN!,
+//     projectId: process.env.REACT_APP_PROJECT_ID!,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET!,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID!,
+//     appId: process.env.REACT_APP_APP_ID!,
+//     measurementId: process.env.REACT_APP_MEASUREMENT_ID!,
+//   };
+  
