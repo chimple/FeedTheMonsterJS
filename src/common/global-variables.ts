0b93716771797c9679453ba121efce5a09fcc129
anuj04patwal <anuj.patwal@sutara.org>
2025-07-09 11:48:39 +0530

    Correct Fallback Logic to handle lang parameter

--- src/common/global-variables.ts
+++ src/common/global-variables.ts
@@ -11,8 +11,8 @@ const supportedLanguages = [
   "afrikaans", "amharic", "arabic", "arabictest", "australianenglish", "azerbaijani", "bangla", "brazilianportuguese", "caboverdecreole", "caboverdeportuguese", "english", "englishwestafrican", "farsi", "farsitest", "french", "georgian", "gujarati", "haitiancreole", "hausa", "hindi", "igbo", "indianenglish", "isixhosa", "javanese", "kannada", "kinyarwanda", "kirundi", "kurdish", "lugandan", "malay", "malgache", "marathi", "ndebele", "nepali", "oromo", "pashto", "punjabi", "sepedi", "sesotho", "shona", "siswati", "somali", "southafricanenglish", "spanish", "swahili", "tagalog", "tajik", "tamil", "telugu", "thai", "tigragna", "tsonga", "tswana", "turkish", "twi", "ukrainian", "venda", "vietnamese", "wolof", "yoruba", "zulu"
 ];
 
-let urlLang = urlParams.get("lang");
-export var lang = supportedLanguages.includes(urlLang.toLowerCase()) ? urlLang.toLowerCase() : "english";
+let urlLang = urlParams.get("lang") != null ? urlParams.get("lang").toLowerCase() : null;
+export var lang = supportedLanguages.includes(urlLang) ? urlLang : "english";
 
 export var lesson_id = urlParams.get("lesson_id") == null || urlParams.get("lesson_id") == "0" ? "1" : urlParams.get("lesson_id");
 
